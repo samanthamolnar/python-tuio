691b3e07aced2df12e5f373e4db832a2f64915c3
Samantha molnar <smolnar9310@gmail.com>
2022-08-01 12:12:03 -0600

    Update const.py

--- pythontuio/const.py
+++ pythontuio/const.py
@@ -4,7 +4,7 @@ File for constant TUIO values
 """
 TUIO_CURSOR = "/tuio/2Dcur"
 TUIO_OBJECT = "/tuio/2Dobj"
-TUIO_BLOB   = "/tuio/2Dblb"
+TUIO_BLOB   = "/tuio/2Dobj"
 
 TUIO_ALIVE = "alive"
 TUIO_SET =  "set"
